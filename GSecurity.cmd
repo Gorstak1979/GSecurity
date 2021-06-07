8d4e4ca9bbc39dbc04fc695353ee59c82956268c
Gorstak1979 <84965902+Gorstak1979@users.noreply.github.com>
2021-06-07 14:11:46 +0200

    Add files via upload

--- GSecurity.cmd
+++ GSecurity.cmd
@@ -1,33 +1,16 @@
 @Echo off
 Title GSecurity
 Color 0b
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\icacls.exe" "%SYSTEMROOT%\system32\config\system"
+NET SESSION
+IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
+GOTO ADMINTASKS
 
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
+:ELEVATE
+CD /d %~dp0
+MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();"
+EXIT
 
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    echo args = "" >> "%temp%\getadmin.vbs"
-    echo For Each strArg in WScript.Arguments >> "%temp%\getadmin.vbs"
-    echo args = args ^& strArg ^& " "  >> "%temp%\getadmin.vbs"
-    echo Next >> "%temp%\getadmin.vbs"
-    echo UAC.ShellExecute "%~s0", args, "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs" %*
-    exit /B
-
-:gotAdmin
-    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
+:ADMINTASKS
 :: Make current folder active one
 pushd %~dp0
 :: Remove user account
