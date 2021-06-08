776f8d39bb3d2314ba558a6f2120ee45671214c4
Gorstak1979 <84965902+Gorstak1979@users.noreply.github.com>
2021-06-08 02:13:32 +0200

    Add files via upload

--- GSecurity.cmd
+++ GSecurity.cmd
@@ -1,16 +1,43 @@
-@Echo off
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 Title GSecurity
 Color 0b
-NET SESSION
-IF %ERRORLEVEL% NEQ 0 GOTO ELEVATE
-GOTO ADMINTASKS
-
-:ELEVATE
-CD /d %~dp0
-MSHTA "javascript: var shell = new ActiveXObject('shell.application'); shell.ShellExecute('%~nx0', '', '', 'runas', 1);close();"
-EXIT
-
-:ADMINTASKS
 :: Make current folder active one
 pushd %~dp0
 :: Remove user account
@@ -139,7 +166,7 @@ Reg.exe import GSecurity.reg
 ren GSecurity.reg GSecurity01.bin
 :: Appz
 ren GSecurity02.bin KeyScrambler_Setup.exe
-Start /wait "KeyScrambler_Setup.exe" /S
+Start /wait "" "KeyScrambler_Setup.exe" /S
 ren KeyScrambler_Setup.exe GSecurity02.bin
 :: Exit
 popd
